e75843daf58f94b069e4ace04b2f5fb82d074133
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2024-08-13 10:13:17 +0200

    Fixes for tests

--- tests/test_scenario_bank_statement_different_currency.py
+++ tests/test_scenario_bank_statement_different_currency.py
@@ -1,15 +1,10 @@
-# =======================================================
-# Account Bank Statement With Different Currency Scenario
-# =======================================================
-
-# Imports
 from trytond.modules.account.tests.tools import create_fiscalyear, create_chart, get_accounts
-from trytond.modules.company.tests.tools import create_company, get_company 
-from trytond.modules.currency.tests.tools import get_currency 
-from trytond.tests.tools import activate_modules 
+from trytond.modules.company.tests.tools import create_company, get_company
+from trytond.modules.currency.tests.tools import get_currency
+from trytond.tests.tools import activate_modules
 from proteus import Model
-from decimal import Decimal 
-import datetime 
+from decimal import Decimal
+import datetime
 import unittest
 from trytond.tests.test_tryton import drop_db
 
@@ -24,20 +19,16 @@ class Test(unittest.TestCase):
 
     def test(self):
 
-        today = datetime.date.today()
         now = datetime.datetime.now()
 
         # Install account_bank_statement and account_invoice
-        config = activate_modules('account_bank_statement')
+        activate_modules('account_bank_statement')
 
         # Create company
         dolar = get_currency('USD')
         eur = get_currency('EUR')
         _ = create_company(currency=eur)
         company = get_company()
-
-        # Reload the context
-        User = Model.get('res.user')
 
         # Create fiscal year
         fiscalyear = create_fiscalyear(company)
@@ -47,8 +38,6 @@ class Test(unittest.TestCase):
         _ = create_chart(company)
         accounts = get_accounts(company)
         receivable = accounts['receivable']
-        revenue = accounts['revenue']
-        expense = accounts['expense']
         cash = accounts['cash']
         cash.bank_reconcile = True
         cash.save()
@@ -97,9 +86,7 @@ class Test(unittest.TestCase):
         line2.credit = Decimal('80.0')
         line2.party = party
         move.click('post')
-        self.assertEqual(move.state
-        , 'posted'
-        )
+        self.assertEqual(move.state, 'posted')
 
         # Create Bank Statement With Different Curreny
         BankStatement = Model.get('account.bank.statement')
@@ -113,16 +100,10 @@ class Test(unittest.TestCase):
         statement_line.description = 'Statement Line'
         statement_line.amount = Decimal('80.0') / Decimal('2.0')
         statement.click('confirm')
-        self.assertEqual(statement.state
-        , 'confirmed'
-        )
+        self.assertEqual(statement.state, 'confirmed')
         statement_line = StatementLine(1)
-        self.assertEqual(statement_line.state
-        , 'confirmed'
-        )
+        self.assertEqual(statement_line.state, 'confirmed')
 
         # Cancel line
         statement_line.click('cancel')
-        self.assertEqual(statement_line.state
-        , 'cancelled'
-        )
+        self.assertEqual(statement_line.state, 'cancelled')
